2275fb4373173bf61ebe09dcb984798df340807e
Andreas Mantzoutas <andreas@osec.io>
2023-11-10 09:27:12 +0200

    Add wasm parsing crate for contractimport

---
+++ stellar/soroban-rs-spec/src/read.rs
@@ -0,0 +1,39 @@
+use std::io::Cursor;
+
+use stellar_xdr::curr as stellar_xdr;
+use stellar_xdr::{DepthLimitedRead, ReadXdr, ScSpecEntry};
+use wasmparser::{BinaryReaderError, Parser, Payload};
+
+pub fn parse_raw(spec: &[u8]) -> Result<Vec<ScSpecEntry>, stellar_xdr::Error> {
+    let cursor = Cursor::new(spec);
+    let entries = ScSpecEntry::read_xdr_iter(&mut DepthLimitedRead::new(cursor, 1000))
+        .collect::<Result<Vec<_>, _>>()?;
+    Ok(entries)
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum FromWasmError {
+    #[error("reading wasm")]
+    Read(BinaryReaderError),
+    #[error("parsing contract spec")]
+    Parse(stellar_xdr::Error),
+    #[error("contract spec not found")]
+    NotFound,
+}
+
+pub fn raw_from_wasm(wasm: &[u8]) -> Result<Vec<u8>, FromWasmError> {
+    for payload in Parser::new(0).parse_all(wasm) {
+        let payload = payload.map_err(FromWasmError::Read)?;
+        if let Payload::CustomSection(section) = payload {
+            if section.name() == "contractspecv0" {
+                return Ok(section.data().to_vec());
+            }
+        };
+    }
+    Err(FromWasmError::NotFound)
+}
+
+pub fn from_wasm(wasm: &[u8]) -> Result<Vec<ScSpecEntry>, FromWasmError> {
+    let spec = raw_from_wasm(wasm)?;
+    parse_raw(&spec).map_err(FromWasmError::Parse)
+}
