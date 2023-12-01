7d7a6a3c80fe2041dcdce7d7b1f041d4a068705e
Andreas Mantzoutas <andreas@osec.io>
2023-12-01 14:28:58 +0200

    Mock tokene events to nop

--- stellar/soroban-token-sdk/src/event.rs
+++ stellar/soroban-token-sdk/src/event.rs
@@ -1,48 +1,25 @@
-use soroban_sdk::{symbol_short, Address, Env, EnvTrait, Symbol};
+use soroban_sdk::{Address, Env};
 
 pub struct Events {
-    env: Env,
+    _env: Env,
 }
 
 impl Events {
     pub fn new(env: &Env) -> Events {
-        Events { env: env.clone() }
+        Events { _env: env.clone() }
     }
 
-    pub fn approve(&self, from: Address, to: Address, amount: i128, expiration_ledger: u32) {
-        let topics = (Symbol::new_from_str("approve"), from, to);
-        self.env
-            .events()
-            .publish(topics, (amount, expiration_ledger));
-    }
+    pub fn approve(&self, _from: Address, _to: Address, _amount: i128, _expiration_ledger: u32) {}
 
-    pub fn transfer(&self, from: Address, to: Address, amount: i128) {
-        let topics = (symbol_short!("transfer"), from, to);
-        self.env.events().publish(topics, amount);
-    }
+    pub fn transfer(&self, _from: Address, _to: Address, _amount: i128) {}
 
-    pub fn mint(&self, admin: Address, to: Address, amount: i128) {
-        let topics = (symbol_short!("mint"), admin, to);
-        self.env.events().publish(topics, amount);
-    }
+    pub fn mint(&self, _admin: Address, _to: Address, _amount: i128) {}
 
-    pub fn clawback(&self, admin: Address, from: Address, amount: i128) {
-        let topics = (symbol_short!("clawback"), admin, from);
-        self.env.events().publish(topics, amount);
-    }
+    pub fn clawback(&self, _admin: Address, _from: Address, _amount: i128) {}
 
-    pub fn set_authorized(&self, admin: Address, id: Address, authorize: bool) {
-        let topics = (Symbol::new_from_str("set_authorized"), admin, id);
-        self.env.events().publish(topics, authorize);
-    }
+    pub fn set_authorized(&self, _admin: Address, _id: Address, _authorize: bool) {}
 
-    pub fn set_admin(&self, admin: Address, new_admin: Address) {
-        let topics = (symbol_short!("set_admin"), admin);
-        self.env.events().publish(topics, new_admin);
-    }
+    pub fn set_admin(&self, _admin: Address, _new_admin: Address) {}
 
-    pub fn burn(&self, from: Address, amount: i128) {
-        let topics = (symbol_short!("burn"), from);
-        self.env.events().publish(topics, amount);
-    }
+    pub fn burn(&self, _from: Address, _amount: i128) {}
 }
